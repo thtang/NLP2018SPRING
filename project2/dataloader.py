3710ac3d56173439f36195df2e433fbf66d5b6ce
thtang <thtang@nlg.csie.ntu.edu.tw>
2018-06-19 19:45:50 +0800

    mod preprocessing

--- project2/dataloader.py
+++ project2/dataloader.py
@@ -8,7 +8,7 @@ def clean_str(string):
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"[^A-Za-z0-9(),\-!?\'\`]", " ", string)
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
